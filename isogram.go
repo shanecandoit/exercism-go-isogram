94903d654a123707f1f7340ea3190c2893faeaa1
shane <shanecandoit@gmail.com>
2019-12-22 22:31:42 -0600

    passes some tests. TODO mix case.

--- isogram.go
+++ isogram.go
@@ -1,1 +1,23 @@
 package isogram
+
+// An isogram (also known as a "nonpattern word") is a word or phrase without a repeating letter, however spaces and hyphens are allowed to appear multiple times.
+
+// IsIsogram
+func IsIsogram(in string) bool {
+
+	if len(in) == 0 {
+		return true
+	}
+
+	chars := make(map[rune]bool)
+	for _, ch := range in {
+
+		if !chars[ch] {
+			chars[ch] = true
+		} else {
+			return false
+		}
+	}
+
+	return true
+}
